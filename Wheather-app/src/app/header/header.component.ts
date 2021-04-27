ee640270aa60f4bc54bda91e11527cee53f51660
Dan Santos <dan.santos@cotalker.com>
2021-04-27 16:21:31 -0400

    Se agrega los elementos basicos del header

--- Wheather-app/src/app/header/header.component.ts
+++ Wheather-app/src/app/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  selectedRegion: String;
+  namesRegions: Array<String>;
 
-  ngOnInit() {
+  constructor() {
+    this.selectedRegion = '';
+    this.namesRegions = ['Valparaiso', 'Metropolitana', 'Bio Bio', 'Araucania'];
   }
 
+  selectRegion(name: String) {
+    this.selectedRegion = name;
+  }
 }
