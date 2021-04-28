29111cbaaaf7d6b5cab8e1e3176b6b63f58f12bf
Dan Santos <dan.santos@cotalker.com>
2021-04-28 13:57:42 -0400

    Se agrega la funcion makeDay en form

--- Wheather-app/src/app/components/weather-form/weather-form.component.ts
+++ Wheather-app/src/app/components/weather-form/weather-form.component.ts
@@ -3,13 +3,15 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HelperService } from 'src/app/services/helper-service.service';
 import { NAMES_REGIONS, NAMES_DAYS } from '../../models/global-names';
+import { WeatherTypes } from '../../models/weather';
+import { Day } from '../../models/day';
 
-export interface formInfo {
+interface formInfo {
   region: number,
-  weather: number,
-  day: number,
+  weather: string,
+  day: string,
   temperature: number,
-  degrees: string,
+  degrees: number,
 }
 
 @Component({
@@ -22,7 +24,7 @@ export class WeatherFormComponent {
   weatherInfo: FormGroup;
 
   namesRegions: Array<string> = [...NAMES_REGIONS];
-  days = NAMES_DAYS;
+  namesDays = NAMES_DAYS;
 
   constructor(
     private _fb: FormBuilder,
@@ -31,17 +33,41 @@ export class WeatherFormComponent {
     ) {
     this.weatherInfo = this._fb.group({
       region: [0, [Validators.required]],
-      weather: [0, [Validators.required]],
-      day: [0, [Validators.required]],
+      weather: ['', [Validators.required]],
+      day: ['', [Validators.required]],
       temperature: [0, [Validators.required]],
-      degrees: ['', [Validators.required]],
+      degrees: [0, [Validators.required]],
     })
   }
 
   onSubmit() {
-    console.log(this.weatherInfo.getRawValue());
-    this.router.navigate(['']);
+    console.log(this.makeDayFromValues(this.weatherInfo.getRawValue()));
+    //this.router.navigate(['']);
   }
 
+  private makeDayFromValues(values: formInfo) {
+    let day: Day = {display: '', tempCelsius: 0, tempFarenheit: 0, weather: WeatherTypes.SOLEADO};
 
+    // check if celsius
+    if (1 == values.degrees) {
+      day.tempCelsius = values.temperature;
+      day.tempFarenheit = this.celsiusToFar(values.temperature);
+    } else {
+      day.tempFarenheit = values.temperature;
+      day.tempCelsius = this.farToCelsius(values.temperature);
+    }
+
+    day.display = values.day;
+    return day;
+  }
+
+  private celsiusToFar(celsius: number) {
+    const far = (celsius * 9 / 5) + 32;
+    return Math.trunc(far);
+  }
+
+  private farToCelsius(far: number) {
+    const celsius = (far - 32) * 5 / 9;
+    return Math.trunc(celsius);
+  }
 }
